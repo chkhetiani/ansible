9192895b0c839699170bec6070ea1885d9557e48
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2024-04-08 17:54:56 +0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 sudo dnf install -y git ansible
 
-ansible-pull -U git@github.com:chkhetiani/ansible.git main.yml
+ansible-pull -U https://github.com/chkhetiani/ansible.git main.yml
 
 # curl -O https://raw.githubusercontent.com/chkhetiani/ansible/master/bootsrap.sh | bash
