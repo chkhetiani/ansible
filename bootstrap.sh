afd791b59f0d38df902cf82bfe82b1f60d4f7329
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2024-04-08 22:02:10 +0400

    fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ sudo dnf install -y git ansible
 
 ansible-pull -U https://github.com/chkhetiani/ansible.git main.yml
 
-# curl -O https://raw.githubusercontent.com/chkhetiani/ansible/master/bootsrap.sh | bash
+# curl -O https://raw.githubusercontent.com/chkhetiani/ansible/master/bootstrap.sh | bash
